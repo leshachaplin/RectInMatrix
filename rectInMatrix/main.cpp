6039354a4a3d0e42b2f66a853c36ee65cf9df782
alexey <lesha.chaplin66@gmail.com>
2017-10-08 20:59:21 +0300

    implemented lab

--- rectInMatrix/main.cpp
+++ rectInMatrix/main.cpp
@@ -1,9 +1,67 @@
-#include <iostream>
+#include<iostream>
+#include<time.h>
+#include<stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    srand(time(0));
+    setlocale(LC_ALL, "RUS");
+    int i, j, N, M;
+    cout << "N=";
+    cin >> N;
+    cout << "M=";
+    cin >> M;
+
+    int matrix[N][M];
+    cout << "Input matrix A \n";
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            matrix[i][j] = rand() % 10;
+        }
+    }
+
+    cout << "matrix A \n";
+    for (i = 0; i < N; i++){
+        for (j = 0; j < M; j++)
+        cout << matrix[i][j]<<" ";
+        cout << endl;
+    }
+
+    int a, b, c, d;
+    cout << "enter coordinates" << endl;
+    cin >> a ;
+    cin >> b ;
+    cin >> c ;
+    cin >> d ;
+    bool isAFound = false;
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            if (matrix[i][j] == a){
+                for (int k = 0; k < M; k++) {
+                    if (k == j) continue;
+
+                    if (matrix[i][k] == b ||
+                        matrix[i][k] == c ||
+                        matrix[i][k] == d) {
+                            int point1 = (matrix[i][k] == b) ? c : b;
+                            int point2 = (matrix[i][k] == c) ? d : c;
+                            for (int x = 0; x < N; x++) {
+                                if (x == i) continue;
+
+                                if ((matrix[x][j] == point1 && matrix[x][k] == point2) ||
+                                    (matrix[x][j] == point2 && matrix[x][k] == point1)) {
+                                    cout << "rectangle found" << endl;
+                                    goto afterLoop;
+                                }
+                            }
+                        }
+                }
+            }
+        }
+    }
+    afterLoop:
+
     return 0;
 }
